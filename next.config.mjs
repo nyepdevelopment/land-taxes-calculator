82a029a6ce605021eb047bd0bf0c2f4ad71d20b6
jbdc-ey <jefferson.dela.cruz@ph.ey.com>
2024-06-28 16:23:49 +0800

    rev

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	basePath: "/land-taxes-calculator",
-	output: "export", // <=== enables static exports
-	reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
