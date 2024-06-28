ad37a04c2ffdcf5e5255141be49da70a6599192a
jbdc-ey <jefferson.dela.cruz@ph.ey.com>
2024-06-28 16:23:28 +0800

    rev

--- out/_next/static/b0SHTGpYy50tvEZdQpROb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/0da66dae06b57817.css","static/chunks/pages/index-8ddaeb6e27abcfac.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
