4d7785f6031f9d4605a6718d5aea595b1fdc7d9a
jbdc-ey <jefferson.dela.cruz@ph.ey.com>
2024-06-28 18:18:35 +0800

    rev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,44 +22,57 @@ export default function Home() {
 	return (
 		<Container>
 			<div className="max-w-[500px] mx-auto w-full">
-				<h1 className="text-[1.2rem] font-[700] text-center">LAND TAXES CALCULATOR</h1>
-				<InputField className="mt-8" type="number" placeholder="Sale Value (PHP)" value={saleValue} onChange={(e) => setSaleValue(+e.currentTarget.value)} />
-				<InputField className="mt-2" type="number" placeholder="Years of Penalty" value={yearsPenalty} onChange={(e) => setYearsPenalty(+e.currentTarget.value)} />
+				<h1 className="text-[1.4rem] font-[700] text-center">LAND TAXES CALCULATOR</h1>
+				<InputField className="mt-4" type="number" placeholder="Sale Value (in Peso)" defaultValue={saleValue} onChange={(e) => setSaleValue(+e.currentTarget.value)} />
+				<InputField className="mt-2" type="number" placeholder="Penalty (in years)" defaultValue={yearsPenalty} onChange={(e) => setYearsPenalty(+e.currentTarget.value)} />
 
-				<h2 className="mt-4 text-[1rem] font-[700]">BASIC TAXES</h2>
+				<h2 className="mt-4 text-[1.25rem] font-[700]">BASIC TAXES</h2>
+				<hr className="mt-1 border-black" />
 				<div className="mt-2 flex flex-col gap-1">
-					<p>= CGT&#40;6% of Sale&#41; + DST&#40;1.5% of Sale&#41; x 20% x Years of Penalty</p>
+					<p>
+						= <b title={cgt.toString()}>CGT&#40;6% of Sale&#41;</b> + <b title={dst.toString()}>DST&#40;1.5% of Sale&#41;</b> x <b>20%</b> x{" "}
+						<b title={yearsPenalty.toString()}>Years of Penalty</b>
+					</p>
 					<p>
 						= <u>{basicTaxes}</u>
 					</p>
 				</div>
 
-				<h2 className="mt-4 text-[1rem] font-[700]">COMPROMISE</h2>
+				<h2 className="mt-4 text-[1.25rem] font-[700]">SURCHARGE</h2>
+				<hr className="mt-1 border-black" />
 				<div className="mt-2 flex flex-col gap-1">
-					<CompromiseTable basicTaxes={basicTaxes} />
-				</div>
-
-				<h2 className="mt-4 text-[1rem] font-[700]">SURCHARGE</h2>
-				<div className="mt-2 flex flex-col gap-1">
-					<p>= BASIC TAXES x 25%</p>
+					<p>
+						= <b title={basicTaxes.toString()}>BASIC TAXES</b> x <b>25%</b>
+					</p>
 					<p>
 						= <u>{surcharge}</u>
 					</p>
 				</div>
 
-				<h2 className="mt-4 text-[1rem] font-[700]">PENALTIES</h2>
+				<h2 className="mt-4 text-[1.25rem] font-[700]">COMPROMISE</h2>
 				<div className="mt-2 flex flex-col gap-1">
-					<p>= BASIC TAXES + COMPROMISE + SURCHARGE</p>
+					<CompromiseTable basicTaxes={basicTaxes} />
+				</div>
+
+				<h2 className="mt-4 text-[1.25rem] font-[700]">PENALTIES</h2>
+				<hr className="mt-1 border-black" />
+				<div className="mt-2 flex flex-col gap-1">
+					<p>
+						= <b title={basicTaxes.toString()}>BASIC TAXES</b> + <b title={surcharge.toString()}>SURCHARGE</b> + <b title={compromise.toString()}>COMPROMISE</b>
+					</p>
 					<p>
 						= <u>{penalties}</u>
 					</p>
 				</div>
 
-				<h2 className="mt-4 text-[1rem] font-[700]">OVERALL</h2>
+				<h2 className="mt-4 text-[1.25rem] font-[700]">OVERALL</h2>
+				<hr className="mt-1 border-black" />
 				<div className="mt-2 flex flex-col gap-1">
-					<p>= PENALTIES + BASIC TAXES</p>
 					<p>
-						= <u>{penalties + basicTaxes}</u>
+						= <b title={basicTaxes.toString()}>BASIC TAXES</b> + <b title={penalties.toString()}>PENALTIES</b>
+					</p>
+					<p>
+						= <u>{basicTaxes + penalties}</u>
 					</p>
 				</div>
 			</div>
