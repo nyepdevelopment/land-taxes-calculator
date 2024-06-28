cb1b3fb3c22c2ab8305b7255ff7c5453102bb8d1
jbdc-ey <jefferson.dela.cruz@ph.ey.com>
2024-06-28 22:27:32 +0800

    rev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Container from "../components/Container";
 import "../app/globals.css";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<Container>
 			<h1 className="text-[1.6rem] font-[700] text-center">LAND TAXES CALCULATOR</h1>
 			<nav className="mt-4 flex flex-col items-center">
-				<a className="px-4 py-2 border border-black hover:bg-black duration-300 hover:text-white" href="/register-deed-taxes">Register Deed Taxes</a>
+				<Link className="px-4 py-2 border border-black hover:bg-black duration-300 hover:text-white" href="/register-deed-taxes">Register Deed Taxes</Link>
 			</nav>
 		</Container>
 	);
