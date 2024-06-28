1c26414fc87904f5692e28a23b330d4b4d233d97
jbdc-ey <jefferson.dela.cruz@ph.ey.com>
2024-06-28 20:52:57 +0800

    rev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,26 @@ export default function Home() {
 
 	return (
 		<Container>
-			<div className="max-w-[500px] mx-auto w-full">
+			<div className="max-w-[600px] mx-auto w-full">
 				<h1 className="text-[1.4rem] font-[700] text-center">LAND TAXES CALCULATOR</h1>
-				<InputField name="saleValue" className="mt-4" type="number" label="Sale Value" placeholder="0" defaultValue={0} onChange={(e) => setSaleValue(+e.currentTarget.value)} />
-				<InputField name="yearsPenalty" className="mt-2" type="number" label="Years Penalty" placeholder="0" defaultValue={0} onChange={(e) => setYearsPenalty(+e.currentTarget.value)} />
+				<InputField
+					name="saleValue"
+					className="mt-4"
+					type="number"
+					label="Sale Value"
+					placeholder="0"
+					defaultValue={0}
+					onChange={(e) => setSaleValue(+e.currentTarget.value)}
+				/>
+				<InputField
+					name="yearsPenalty"
+					className="mt-2"
+					type="number"
+					label="Years Penalty"
+					placeholder="0"
+					defaultValue={0}
+					onChange={(e) => setYearsPenalty(+e.currentTarget.value)}
+				/>
 
 				<h2 className="mt-4 text-[1.25rem] font-[700]">BASIC TAXES</h2>
 				<hr className="mt-1 border-black" />
@@ -33,9 +49,7 @@ export default function Home() {
 						= <b title={ValueToPeso(cgt)}>CGT&#40;6% of Sale&#41;</b> + <b title={ValueToPeso(dst)}>DST&#40;1.5% of Sale&#41;</b> x <b>20%</b> x{" "}
 						<b title={yearsPenalty.toString()}>Years of Penalty</b>
 					</p>
-					<p>
-						= {ValueToPeso(basicTaxes)}
-					</p>
+					<p>= {ValueToPeso(basicTaxes)}</p>
 				</div>
 
 				<h2 className="mt-4 text-[1.25rem] font-[700]">SURCHARGE</h2>
@@ -44,9 +58,7 @@ export default function Home() {
 					<p>
 						= <b title={ValueToPeso(basicTaxes)}>BASIC TAXES</b> x <b>25%</b>
 					</p>
-					<p>
-						= {ValueToPeso(surcharge)}
-					</p>
+					<p>= {ValueToPeso(surcharge)}</p>
 				</div>
 
 				<h2 className="mt-4 text-[1.25rem] font-[700]">COMPROMISE</h2>
@@ -60,9 +72,7 @@ export default function Home() {
 					<p>
 						= <b title={ValueToPeso(basicTaxes)}>BASIC TAXES</b> + <b title={ValueToPeso(surcharge)}>SURCHARGE</b> + <b title={ValueToPeso(compromise)}>COMPROMISE</b>
 					</p>
-					<p>
-						= {ValueToPeso(penalties)}
-					</p>
+					<p>= {ValueToPeso(penalties)}</p>
 				</div>
 
 				<h2 className="mt-4 text-[1.25rem] font-[700]">OVERALL</h2>
@@ -71,9 +81,13 @@ export default function Home() {
 					<p>
 						= <b title={ValueToPeso(basicTaxes)}>BASIC TAXES</b> + <b title={ValueToPeso(penalties)}>PENALTIES</b>
 					</p>
-					<p>
-						= {ValueToPeso(basicTaxes + penalties)}
-					</p>
+					<p>= {ValueToPeso(basicTaxes + penalties)}</p>
+				</div>
+
+				<h2 className="mt-4 text-[1.25rem] font-[700]">REFERENCE</h2>
+				<hr className="mt-1 border-black" />
+				<div className="mt-4 flex flex-col gap-1">
+					<iframe width="100%" height="315" src="https://www.youtube.com/embed/Ma0cCJN09aM?si=0PiP3lo4fXf4YtP7" allowFullScreen/>
 				</div>
 			</div>
 		</Container>
